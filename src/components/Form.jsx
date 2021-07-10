4715ddd38f734f3c4f256f9174ab76c0cf57869b
Matt <heffa2004@gmail.com>
2021-07-10 23:10:47 +1000

    UX/UI Changes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,35 +6,88 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { v4 as uuidv4 } from 'uuid';
 import { saveAs } from 'file-saver';
 import '../styles/form.css';
-// import { RMIUploader } from 'react-multiple-image-uploader';
 
 function Form() {
     // let [images, setImages] = useState(null);
     // let [imageName, setImageName] = useState(null);
     // let images = document.querySelector('#image-id').files;
     // let imageName = document.querySelector('#image-id').files.name;
-    var insert = require('../index'),
-        prepend = insert.prepend,
-        append = insert.append,
-        after = insert.after,
-        before = insert.before,
-        remove = insert.remove,
-        replace = insert.replace,
-        list = document.getElementById('list'),
-        textList = document.getElementById('textList');
 
     // template name on "Download Pack action"
 
-    let AddAnother = () => {
-        console.log('hello');
+    const submit_click = (e) => {
+        let action_value = document.querySelector('#actions').value;
         let filesInput = document.querySelector('#image-id');
-
-        // let output = document.querySelector('#result');
-
-        // on each click append to file list
+        // on each button click append to file list
         let files = filesInput.files;
 
-        console.log(files);
+        // skin pack name and template name value
+        let skin = document.querySelector('#skin-name').value;
+        let temp = document.querySelector('#temp-name').value;
+
+        // images
+        // let images = document.querySelector('#image-id').files[0];
+        // let imageName = document.querySelector('#image-id').files[0].name;
+
+        // version number input value
+        let version = document.querySelector('#version').value;
+        version = parseInt(version, 10);
+
+        // execute specific action based on action value selected
+        switch (action_value) {
+            case 'Download Pack':
+                //code block
+                console.log('dp');
+
+                // On submit ask for template name
+                break;
+            case 'New Template':
+                console.log('NT');
+
+                // clear file list array
+                skin = 'yo';
+                temp = '';
+                filesInput.value = '';
+
+                break;
+            case 'Add Another Skin':
+                console.log('add another');
+
+                for (let i = 0; i < files.length; i++) {
+                    var file = files[i];
+                    console.log(files);
+                    if (!file.type.match('image')) {
+                        continue;
+                    }
+                    var picReader = new FileReader();
+                    picReader.addEventListener('load', function (event) {
+                        var picFile = event.target;
+                        let row = document.querySelector('.row.display');
+
+                        let add = `<div className="col-sm" ><img class = "thumbnail" src="${picFile.result}" title="${picFile.name}" /></div>`;
+
+                        row.innerHTML += add;
+                    });
+                    // from here need to pass this to the zip packing
+                    picReader.readAsDataURL(file);
+                }
+
+                break;
+            default:
+                e.preventDefault();
+                console.log('please enter info');
+        }
+    };
+
+    let AddAnother = () => {
+        // add value check here (check if file name matches one in file list)
+
+        let filesInput = document.querySelector('#image-id');
+
+        // on each button click append to file list
+        let files = filesInput.files;
+
+        // console.log(files);
 
         for (let i = 0; i < files.length; i++) {
             var file = files[i];
@@ -55,8 +108,6 @@ function Form() {
             picReader.readAsDataURL(file);
         }
     };
-
-    // read image
 
     let handleButtonClick = () => {
         // e.preventDefault();
@@ -144,7 +195,7 @@ function Form() {
                             aria-describedby="helpId"
                             placeholder=""
                         />
-                        <label htmlFor="template">Template Name</label>
+                        <label htmlFor="template">Skin Pack Name</label>
                         <input
                             type="text"
                             className="form-control"
@@ -185,14 +236,33 @@ function Form() {
                             // multiple
                         />
 
-                        {/* <button
-                    type="submit"
-                    className="btn btn-primary"
-                    onClick={handleButtonClick}
-                >
-                    Submit
-                </button> */}
+                        <label htmlFor="action select">Select Action</label>
+                        <br></br>
+                        {/* Select Action List  */}
+                        <select id="actions" name="cars" size="5">
+                            <option value="Add Another Skin">
+                                Add Another Skin
+                            </option>
+                            <option value="Download Pack">Download Pack</option>
+                            <option value="New Template">
+                                Create New Pack
+                            </option>
+                            {/* <option value="audi">Audi</option> */}
+                        </select>
 
+                        <br />
+                        <br />
+
+                        {/* Submit Button  */}
+
+                        <button
+                            type="submit"
+                            className="btn btn-primary"
+                            onClick={submit_click}
+                        >
+                            Submit
+                        </button>
+                        {/* 
                         <button
                             type="submit"
                             className="btn btn-secondary"
@@ -207,6 +277,10 @@ function Form() {
                         >
                             Download Pack
                         </button>
+
+                        <button className=" btn btn-warning">
+                            New Template
+                        </button> */}
                     </div>
                 </div>
             </div>
