27fa024a0ce3cb2b9ac4d2c59043a997793c6884
Matt <heffa2004@gmail.com>
2021-10-06 23:04:38 +1100

    formatted batch script and fixed user message

--- removeSkins.bat
+++ removeSkins.bat
@@ -7,30 +7,29 @@ for /F %%A in ('dir /b /a %_folder%') Do (
     echo The folder is NOT empty
     goto :List
 )
-echo the folder is empty
-goto :Exit
+    echo the custom installed skins folder is empty
+    goto :Exit
 
 :List
- dir /B "%AppData%\Minecraft Education Edition\games\com.mojang\skin_packs"    
-set /p skinName=please enter pack name to delete:
-@REM set result=false
-set skinPath=%AppData%\Minecraft Education Edition\games\com.mojang\skin_packs
-CALL :Remove
+    dir /B "%AppData%\Minecraft Education Edition\games\com.mojang\skin_packs"    
+    set /p skinName=please enter pack name to delete:
+    set skinPath=%AppData%\Minecraft Education Edition\games\com.mojang\skin_packs
+    CALL :Remove
 Exit /B %ERRORLEVEL% 
 
 :Remove
-cd %skinPath%
-rmdir /s "%skinPath%\%skinName%"
-cd %var%
-echo %skinName% deleted
-dir /B "%AppData%\Minecraft Education Edition\games\com.mojang\skin_packs"
-CALL :Another
+    cd %skinPath%
+    rmdir /s "%skinPath%\%skinName%"
+    cd %var%
+    echo %skinName% deleted
+    dir /B "%AppData%\Minecraft Education Edition\games\com.mojang\skin_packs"
+    CALL :Another
 Exit /B 0
 
 :Another
-set /p delAnother=would you like to remove another skin Y/N?
-if %delAnother% ==y (CALL :List) else (goto :Exit)
+    set /p delAnother=would you like to remove another skin Y/N?
+    if %delAnother% ==y (CALL :List) else (goto :Exit)
 
 :Exit 
-echo done
+    echo done
 pause
